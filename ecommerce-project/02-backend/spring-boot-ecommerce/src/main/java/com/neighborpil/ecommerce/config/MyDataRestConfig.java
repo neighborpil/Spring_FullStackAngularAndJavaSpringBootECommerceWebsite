6fa467f765c1111a737437dfb9e51c673aafe8a8
neighborpil <neighborpil@naver.com>
2022-05-21 09:38:21 +0900

    save

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/neighborpil/ecommerce/config/MyDataRestConfig.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/neighborpil/ecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,25 @@ package com.neighborpil.ecommerce.config;
 
 import com.neighborpil.ecommerce.entity.Product;
 import com.neighborpil.ecommerce.entity.ProductCategory;
+import org.hibernate.type.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager) {
+        this.entityManager = theEntityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -27,5 +38,17 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        // call an internal helper method
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        // expose entity ides
+
+        // get a list of all entity classes from the entity manager
+        set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
     }
 }
