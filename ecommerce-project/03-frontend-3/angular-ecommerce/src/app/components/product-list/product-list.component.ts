a7960550c9885e4eab44554b2266c1f4d5418af5
neighborpil <neighborpil@naver.com>
2022-05-28 07:54:02 +0900

    pagination size

--- ecommerce-project/03-frontend-3/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce-project/03-frontend-3/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -101,4 +101,10 @@ export class ProductListComponent implements OnInit {
       this.theTotalElements = data.page.totalElements;
     };
   }
+
+  updatePageSize(pageSize: number) {
+    this.thePageSize = pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
 }
